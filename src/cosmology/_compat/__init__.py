144b0f2a181a6b913d854e17794e9fbc38a435b8
nstarman <nstarkman@protonmail.com>
2023-01-19 10:08:15 -0500

    refactor as an aggregation llibrary
    Signed-off-by: nstarman <nstarkman@protonmail.com>

---
+++ src/cosmology/_compat/__init__.py
@@ -0,0 +1,6 @@
+"""The Cosmology API compatability libraries.
+
+This is a private module, and is not intended to be used by users.
+Installing the package will automatically install all the compatability
+libraries.
+"""

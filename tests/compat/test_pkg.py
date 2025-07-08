e7ba4a4fd79d5f83b448f95bb1e231b7c56d4d57
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-07-08 04:22:50 +0000

    refactor: move imports to the top of the test file for clarity

--- tests/compat/test_pkg.py
+++ tests/compat/test_pkg.py
@@ -1,15 +1,16 @@
 """Test the Cosmology API compat library."""
 
+import cosmology.compat
+import cosmology.compat.astropy
+
 
 def test_imported():
     """This is a namespace package, so it should be importable."""
-    import cosmology.compat
 
     assert cosmology.compat.__name__ == "cosmology.compat"
 
 
 def test_collected_submodules():
     """Submodules should be importable."""
-    import cosmology.compat.astropy
 
     assert cosmology.compat.astropy.__name__ == "cosmology.compat.astropy"
